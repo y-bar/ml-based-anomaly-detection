ccf449307ee8cfc496994020e51868f9063600b3
takahiro-yoshinaga <takahiro.yoshinaga@linecorp.com>
2019-09-25 22:11:11 +0900

    add

---
+++ .pypirc.py
@@ -0,0 +1,15 @@
+[distutils]
+index-servers =
+    pypi
+    testpypi
+
+# production
+[pypi]
+username: yoshinaga0106
+password: tekenuko.41
+
+# test
+[testpypi]
+repository: https://test.pypi.org/legacy/
+username: yoshinaga0106
+password: tekenuko.41
